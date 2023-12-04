3bf53dcfc5e031fc96fcb7164a289c7ee7f5b8ef
lacoste-Pierre <lacostepierreip@gmail.com>
2023-12-04 11:22:29 +0100

    Class Request

--- src/main/java/tp/vrp/Request.java
+++ src/main/java/tp/vrp/Request.java
@@ -1,16 +1,30 @@
 package tp.vrp;
 
 public class Request {
-    private float quantity ;
+    private double quantity ;
     private Node node ;
     private int id;
-    public float getQuantity() {
+    public double getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(float quantity) {
+    public void setQuantity(double quantity) {
         this.quantity = quantity;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
 }
