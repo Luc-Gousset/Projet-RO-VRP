126a8a8dbdcb34b168301cf18bcf68a3eada7db3
lien1 <brotclement@gmail.com>
2023-12-04 11:14:24 +0100

    V2parser

--- src/main/java/tp/vrp/Main.java
+++ src/main/java/tp/vrp/Main.java
@@ -1,6 +1,15 @@
 package tp.vrp;
 
+import java.util.List;
+
 public class Main {
-        XMLParser parser = new XMLParser();
-        parser.parseXMLFile("path/to/your/file.xml"); // Remplacez par le chemin de votre fichier
+        public static void main(String[] args) {
+                XMLParser parser = new XMLParser();
+                parser.parseXMLFile("C:/Users/brotc/Downloads/JDD03.xml"); // Remplacez par le chemin de votre fichier
+
+                List<String> data = parser.getData();
+                for (int i = 0; i < Math.min(10, data.size()); i++) {
+                        System.out.println(data.get(i));
+                }
+        }
 }
--- src/main/java/tp/vrp/XMLParser.java
+++ src/main/java/tp/vrp/XMLParser.java
@@ -6,27 +6,41 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class XMLParser {
 
-    public void parseXMLFile(String filePath) {
-        try {
-            File xmlFile = new File(filePath);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
 
-            NodeList nList = doc.getElementsByTagName("yourElementTag"); // Remplacez par le nom de votre balise
+    public class XMLParser {
+        private List<String> data;
 
-            for (int i = 0; i < Math.min(10, nList.getLength()); i++) {
-                Node node = nList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    System.out.println(node.getTextContent());
+        public XMLParser() {
+            this.data = new ArrayList<>();
+        }
+
+        public void parseXMLFile(String filePath) {
+            try {
+                File xmlFile = new File(filePath);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(xmlFile);
+                doc.getDocumentElement().normalize();
+
+                NodeList nList = doc.getElementsByTagName("yourElementTag"); // Remplacez par le nom de votre balise
+
+                for (int i = 0; i < nList.getLength(); i++) {
+                    Node node = nList.item(i);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        data.add(node.getTextContent());
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+
+        public List<String> getData() {
+            return data;
         }
     }
-}
+
