d774d8bd657e7a4512bf6f9c8cd4df903c2e4c0a
lacoste-Pierre <lacostepierreip@gmail.com>
2023-12-04 11:50:48 +0100

    Class Vahicule

--- src/main/java/tp/vrp/Vehicule.java
+++ src/main/java/tp/vrp/Vehicule.java
@@ -1,5 +1,12 @@
 package tp.vrp;
 
+
+
+/**
+ * La classe Vehicule représente un véhicule dans le contexte d'un problème de routage de véhicules.
+ * Elle stocke les informations sur le profil du véhicule, le nœud de départ, le nœud d'arrivée,
+ * ainsi que sa capacité initiale et actuelle.
+ */
 public class Vehicule {
 
     private int vehicleProfile;
@@ -9,39 +16,71 @@ public class Vehicule {
     private int capacityInitial ;
     private int capacityCurrent ;
 
+    /**
+     * Définit le nœud d'arrivée du véhicule.
+     *
+     * @param arrivalNode Le nœud d'arrivée à définir.
+     */
     public void setArrivalNode(Node arrivalNode) {
         this.arrivalNode = arrivalNode;
     }
 
+    /**
+     * Définit la capacité initiale du véhicule et initialise la capacité actuelle à cette valeur.
+     *
+     * @param capacityInitial La capacité initiale à définir.
+     */
     public void setCapacityInitial(int capacityInitial) {
         this.capacityInitial = capacityInitial;
         this.capacityCurrent = capacityInitial;
     }
 
+    /**
+     * Définit la capacité actuelle du véhicule.
+     *
+     * @param capacityCurrent La capacité actuelle à définir.
+     */
     public void setCapacityCurrent(int capacityCurrent) {
         this.capacityCurrent = capacityCurrent;
     }
-
+    /**
+     * Ajoute une certaine capacité à la capacité actuelle du véhicule si possible.
+     *
+     * @param capacityToAdd La capacité à ajouter.
+     */
     public void addCapacityCurrent (int capacityToAdd){
         if (canWeAddCapacityCurrent(capacityToAdd)){
             this.capacityCurrent = this.capacityCurrent + capacityToAdd ;
         }
     }
-
+    /**
+     * Vérifie si une certaine capacité peut être ajoutée à la capacité actuelle du véhicule.
+     *
+     * @param capacityToAdd La capacité à vérifier.
+     * @return true si la capacité peut être ajoutée, false sinon.
+     */
     public boolean canWeAddCapacityCurrent(int capacityToAdd){
         return !(this.capacityCurrent + capacityToAdd > this.capacityInitial | this.capacityCurrent + capacityToAdd < 0);
 
     }
-
+    /**
+     * Définit le nœud de départ du véhicule.
+     *
+     * @param departureNode Le nœud de départ à définir.
+     */
     public void setDepartureNode(Node departureNode) {
         this.departureNode = departureNode;
     }
 
-
+    /**
+     * Définit le profil du véhicule.
+     *
+     * @param vehicleProfile Le profil du véhicule à définir.
+     */
     public void setVehicleProfile(int vehicleProfile) {
         this.vehicleProfile = vehicleProfile;
     }
-
+    // Les méthodes suivantes sont des getters pour les attributs de la classe.
     public int getCapacityInitial() {
         return capacityInitial;
     }
