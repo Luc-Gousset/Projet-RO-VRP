bd413726e849a47aaebe4720149576757327e626
lacoste-Pierre <lacostepierreip@gmail.com>
2023-12-04 11:53:02 +0100

    Class rEQUEST

--- src/main/java/tp/vrp/Request.java
+++ src/main/java/tp/vrp/Request.java
@@ -1,29 +1,61 @@
 package tp.vrp;
 
+
+/**
+ * La classe Request représente une requête dans le cadre d'un problème de routage de véhicules.
+ * Elle contient des informations sur la quantité demandée, le nœud associé à la requête,
+ * ainsi qu'un identifiant unique pour chaque requête.
+ */
 public class Request {
     private double quantity ;
     private Node node ;
     private int id;
+
+    /**
+     * Récupère la quantité demandée par la requête.
+     *
+     * @return La quantité demandée.
+     */
     public double getQuantity() {
         return quantity;
     }
-
+    /**
+     * Définit la quantité demandée par la requête.
+     *
+     * @param quantity La quantité à définir.
+     */
     public void setQuantity(double quantity) {
         this.quantity = quantity;
     }
-
+    /**
+     * Définit l'identifiant de la requête.
+     *
+     * @param id L'identifiant à définir.
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * Récupère l'identifiant de la requête.
+     *
+     * @return L'identifiant de la requête.
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Récupère le nœud associé à la requête.
+     *
+     * @return Le nœud associé.
+     */
     public Node getNode() {
         return node;
     }
-
+    /**
+     * Définit le nœud associé à la requête.
+     *
+     * @param node Le nœud à définir.
+     */
     public void setNode(Node node) {
         this.node = node;
     }
